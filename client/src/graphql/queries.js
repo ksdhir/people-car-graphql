193ab1acd67255b51e7a5b84d1a20d831ae36fcf
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-10-26 16:06:04 -0700

    add queries

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,8 +1,17 @@
 import { gql } from '@apollo/client'
 
-export const GET_CONTACTS = gql`
+export const GET_PEOPLE = gql`
   {
-    contacts {
+    people {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+export const GET_PERSON = gql`
+  query GetPerson($id: String!) {
+    person(id: $id) {
       id
       firstName
       lastName
@@ -10,9 +19,19 @@ export const GET_CONTACTS = gql`
   }
 `
 
-export const ADD_CONTACT = gql`
-  mutation AddContact($id: String!, $firstName: String!, $lastName: String!) {
-    addContact(id: $id, firstName: $firstName, lastName: $lastName) {
+export const ADD_PERSON = gql`
+  mutation AddPerson($id: String!, $firstName: String!, $lastName: String!) {
+    addPerson(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+ `
+
+export const UPDATE_PERSON = gql`
+  mutation UpdatePerson($id: String!, $firstName: String!, $lastName: String!) {
+    updatePerson(id: $id, firstName: $firstName, lastName: $lastName) {
       id
       firstName
       lastName
@@ -20,9 +39,9 @@ export const ADD_CONTACT = gql`
   }
 `
 
-export const UPDATE_CONTACT = gql`
-  mutation UpdateContact($id: String!, $firstName: String!, $lastName: String!) {
-    updateContact(id: $id, firstName: $firstName, lastName: $lastName) {
+export const REMOVE_PERSON = gql`
+  mutation RemovePerson($id: String!) {
+    removePerson(id: $id) {
       id
       firstName
       lastName
@@ -30,12 +49,94 @@ export const UPDATE_CONTACT = gql`
   }
 `
 
-export const REMOVE_CONTACT = gql`
-  mutation RemoveContact($id: String!) {
-    removeContact(id: $id) {
+export const GET_CARS_OF_PERSON = gql`
+  query GetCarsOfPerson($personId: String!) {
+    carsOfPerson(personId: $personId) {
       id
-      firstName
-      lastName
+      year
+      make
+      model
+      price
     }
   }
 `
+
+export const ADD_CAR = gql`
+  mutation AddCar($id: String!, $year: String!, $make: String!, $model: String!, $price: String!, $personId: String!) {
+    addCar(id: $id, year: $year, make: $make, model: $model, price: $price, personId: $personId) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
+
+export const UPDATE_CAR = gql`
+  mutation UpdateCar($id: String!, $year: String!, $make: String!, $model: String!, $price: String!, $personId: String!) {
+    updateCar(id: $id, year: $year, make: $make, model: $model, price: $price, personId: $personId) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
+
+export const REMOVE_CAR = gql`
+  mutation RemoveCar($id: String!) {
+    removeCar(id: $id) {
+      id
+      year
+      make
+      model
+      price
+      personId
+    }
+  }
+`
+
+
+// export const GET_CONTACTS = gql`
+//   {
+//     contacts {
+//       id
+//       firstName
+//       lastName
+//     }
+//   }
+// `
+
+// export const ADD_CONTACT = gql`
+//   mutation AddContact($id: String!, $firstName: String!, $lastName: String!) {
+//     addContact(id: $id, firstName: $firstName, lastName: $lastName) {
+//       id
+//       firstName
+//       lastName
+//     }
+//   }
+// `
+
+// export const UPDATE_CONTACT = gql`
+//   mutation UpdateContact($id: String!, $firstName: String!, $lastName: String!) {
+//     updateContact(id: $id, firstName: $firstName, lastName: $lastName) {
+//       id
+//       firstName
+//       lastName
+//     }
+//   }
+// `
+
+// export const REMOVE_CONTACT = gql`
+//   mutation RemoveContact($id: String!) {
+//     removeContact(id: $id) {
+//       id
+//       firstName
+//       lastName
+//     }
+//   }
+// `
